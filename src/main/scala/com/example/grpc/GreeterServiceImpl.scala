7839e717528039836db19fd885d02f530f9bb6dc
莫非 <fei.mo@ascendex.io>
2022-04-08 17:20:24 +0800

    monitor grpc client demo

--- src/main/scala/com/example/grpc/GreeterServiceImpl.scala
+++ src/main/scala/com/example/grpc/GreeterServiceImpl.scala
@@ -3,11 +3,12 @@ package com.example.grpc
 import akka.NotUsed
 import akka.actor.typed.ActorSystem
 import akka.stream.scaladsl.{BroadcastHub, Keep, MergeHub, Sink, Source}
+import com.example.dao.Logable
 import com.example.grpc.helloworld.{GreeterService, HelloReply, HelloRequest}
 
 import scala.concurrent.Future
 
-class GreeterServiceImpl(system: ActorSystem[_]) extends GreeterService {
+class GreeterServiceImpl(system: ActorSystem[_]) extends GreeterService with Logable {
   private implicit val sys: ActorSystem[_] = system
   //#service-request-reply
   val (inboundHub: Sink[HelloRequest, NotUsed], outboundHub: Source[HelloReply, NotUsed]) =
@@ -21,6 +22,7 @@ class GreeterServiceImpl(system: ActorSystem[_]) extends GreeterService {
    * Sends a greeting
    */
   override def sayHello(in: HelloRequest): Future[HelloReply] = {
+    logger.info("grpc server test log.............")
     Future.successful(HelloReply(s"Hello,${in.name}"))
   }
 
